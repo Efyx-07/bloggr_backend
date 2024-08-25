b59d9b628c1f876e3729046b4952ea169185e0cc
François-Xavier Savary <fx.savary@orange.fr>
2024-08-25 11:42:28 +0200

    setup

---
+++ src/app.module.ts
@@ -0,0 +1,30 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOSTNAME'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [],
+        synchronize: true, // disable in production
+      }),
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/entities/article.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity('articles')
+export class Article {
+  @PrimaryGeneratedColumn({ type: 'int', name: 'id' })
+  id: number;
+
+  @Column({ type: 'varchar', length: 255, name: 'image_url' })
+  imageUrl: string;
+
+  @Column({ type: 'varchar', length: 255, name: 'title' })
+  title: string;
+
+  @Column({ type: 'longtext', name: 'body' })
+  body: string;
+
+  @CreateDateColumn({
+    type: 'timestamp',
+    name: 'creation_date',
+  })
+  creationDate: Date;
+}
