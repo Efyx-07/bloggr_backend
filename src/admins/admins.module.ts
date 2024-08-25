25a3254ac4453da6f7d7e22fec51f11a47dc9f77
François-Xavier Savary <fx.savary@orange.fr>
2024-08-25 17:03:20 +0200

    admins

--- src/admins/admins.module.ts
+++ src/admins/admins.module.ts
@@ -3,9 +3,10 @@ import { AdminsController } from './admins.controller';
 import { AdminsService } from './admins.service';
 import { AdminEntity } from 'src/entities/admin.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([AdminEntity])],
+  imports: [TypeOrmModule.forFeature([AdminEntity]), AuthModule],
   controllers: [AdminsController],
   providers: [AdminsService],
 })
