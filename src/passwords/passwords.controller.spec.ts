2b2205d32698c2775cadf378354422209dbf1193
François-Xavier Savary <fx.savary@orange.fr>
2024-08-26 09:25:01 +0200

    ud

--- src/passwords/passwords.controller.spec.ts
+++ src/passwords/passwords.controller.spec.ts
@@ -1,18 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PasswordsController } from './passwords.controller';
+import { PasswordsService } from './passwords.service';
+import UpdatePasswordDto from './passwords-DTO/update-passwords.dto';
+import { InternalServerErrorException } from '@nestjs/common';
+import { AuthGuard } from '../auth/auth.guard'; // Importez votre AuthGuard
 
 describe('PasswordsController', () => {
-  let controller: PasswordsController;
+  let passwordsController: PasswordsController;
+  let passwordsService: PasswordsService;
 
   beforeEach(async () => {
+    const mockAuthGuard = {
+      canActivate: jest.fn(() => true), // Simule l'authentification réussie
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [PasswordsController],
-    }).compile();
+      providers: [
+        {
+          provide: PasswordsService,
+          useValue: {
+            updateAdminPasswordById: jest.fn(),
+          },
+        },
+      ],
+    })
+      .overrideGuard(AuthGuard) // Remplacez l'AuthGuard par le mock
+      .useValue(mockAuthGuard)
+      .compile();
 
-    controller = module.get<PasswordsController>(PasswordsController);
+    passwordsController = module.get<PasswordsController>(PasswordsController);
+    passwordsService = module.get<PasswordsService>(PasswordsService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('updatePassword', () => {
+    it('should return a success message when password is updated', async () => {
+      const dto = new UpdatePasswordDto();
+      (passwordsService.updateAdminPasswordById as jest.Mock).mockResolvedValue(
+        undefined,
+      );
+
+      const result = await passwordsController.updatePassword(dto);
+
+      expect(result).toEqual({ message: 'Password successfully updated' });
+      expect(passwordsService.updateAdminPasswordById).toHaveBeenCalledWith(
+        dto,
+      );
+    });
+
+    it('should throw an InternalServerErrorException when an error occurs', async () => {
+      const dto = new UpdatePasswordDto();
+      (passwordsService.updateAdminPasswordById as jest.Mock).mockRejectedValue(
+        new Error('Unexpected error'),
+      );
+
+      await expect(passwordsController.updatePassword(dto)).rejects.toThrow(
+        InternalServerErrorException,
+      );
+      expect(passwordsService.updateAdminPasswordById).toHaveBeenCalledWith(
+        dto,
+      );
+    });
   });
 });
