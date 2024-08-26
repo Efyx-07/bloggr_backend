bd2a844bbe61654171ea6b3986573c9cd4247fc3
François-Xavier Savary <fx.savary@orange.fr>
2024-08-26 12:42:47 +0200

    test auth

--- src/auth/auth.guard.spec.ts
+++ src/auth/auth.guard.spec.ts
@@ -1,7 +1,97 @@
 import { AuthGuard } from './auth.guard';
+import { JwtService } from '@nestjs/jwt';
+import {
+  ExecutionContext,
+  UnauthorizedException,
+  ForbiddenException,
+} from '@nestjs/common';
 
 describe('AuthGuard', () => {
+  let authGuard: AuthGuard;
+  let jwtService: JwtService;
+
+  beforeEach(() => {
+    jwtService = new JwtService();
+    authGuard = new AuthGuard(jwtService);
+  });
+
   it('should be defined', () => {
-    expect(new AuthGuard()).toBeDefined();
+    expect(authGuard).toBeDefined();
+  });
+
+  it('should throw UnauthorizedException if authorization header is missing', async () => {
+    const mockContext = {
+      switchToHttp: () => ({
+        getRequest: () => ({
+          headers: {},
+        }),
+      }),
+    } as unknown as ExecutionContext;
+
+    await expect(authGuard.canActivate(mockContext)).rejects.toThrow(
+      UnauthorizedException,
+    );
+  });
+
+  it('should throw UnauthorizedException if authorization header is malformed', async () => {
+    const mockContext = {
+      switchToHttp: () => ({
+        getRequest: () => ({
+          headers: {
+            authorization: 'MalformedToken',
+          },
+        }),
+      }),
+    } as unknown as ExecutionContext;
+
+    await expect(authGuard.canActivate(mockContext)).rejects.toThrow(
+      UnauthorizedException,
+    );
+  });
+
+  it('should throw ForbiddenException if token is invalid', async () => {
+    jest
+      .spyOn(jwtService, 'verifyAsync')
+      .mockRejectedValue(new Error('Invalid token'));
+
+    const mockContext = {
+      switchToHttp: () => ({
+        getRequest: () => ({
+          headers: {
+            authorization: 'Bearer invalid.token',
+          },
+        }),
+      }),
+    } as unknown as ExecutionContext;
+
+    await expect(authGuard.canActivate(mockContext)).rejects.toThrow(
+      ForbiddenException,
+    );
+  });
+
+  it('should return true if token is valid', async () => {
+    const mockAdminId = 1;
+    jest
+      .spyOn(jwtService, 'verifyAsync')
+      .mockResolvedValue({ adminId: mockAdminId });
+
+    // Créer un mock pour `request`
+    const mockRequest = {
+      headers: {
+        authorization: 'Bearer valid.token',
+      },
+      admin: undefined,
+    };
+
+    const mockContext = {
+      switchToHttp: () => ({
+        getRequest: () => mockRequest,
+      }),
+    } as unknown as ExecutionContext;
+
+    const result = await authGuard.canActivate(mockContext);
+
+    expect(result).toBe(true);
+    expect(mockRequest.admin).toBe(mockAdminId);
   });
 });
