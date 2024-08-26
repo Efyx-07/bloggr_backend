04ced2b01f5b626c47f0c28f2f009b3f2a3f2e75
François-Xavier Savary <fx.savary@orange.fr>
2024-08-26 11:52:52 +0200

    test auth

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,65 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+  let jwtService: JwtService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      providers: [
+        AuthService,
+        {
+          provide: JwtService,
+          useValue: {
+            sign: jest.fn(),
+            verify: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
+    jwtService = module.get<JwtService>(JwtService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  describe('generateJWTToken', () => {
+    it('should generate a token', async () => {
+      const mockToken = 'mockedToken';
+      const adminId = 1;
+      jest.spyOn(jwtService, 'sign').mockReturnValue(mockToken);
+      const result = authService.generateJWTToken(adminId);
+
+      expect(jwtService.sign).toHaveBeenCalledWith({ adminId });
+      expect(result).toBe(mockToken);
+    });
+  });
+
+  describe('verifyJWTToken', () => {
+    it('should verify token', async () => {
+      const mockPayload = { adminId: 1 };
+      const mockToken = 'mockedToken';
+      jest.spyOn(jwtService, 'verify').mockReturnValue(mockPayload);
+      const result = authService.verifyJWTToken(mockToken);
+
+      expect(jwtService.verify).toHaveBeenCalledWith(mockToken);
+      expect(result).toBe(mockPayload);
+    });
+
+    it('should throw an error if token is invalid', async () => {
+      const mockToken = 'invalidToken';
+      jest.spyOn(jwtService, 'verify').mockImplementation(() => {
+        throw new Error('Invalid token');
+      });
+
+      expect(() =>
+        authService.verifyJWTToken(mockToken).toThrow('Invalid token'),
+      );
+    });
   });
 });
